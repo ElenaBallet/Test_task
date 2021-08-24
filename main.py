ba9faec44e49d90e11e724df66847eb71b49cd79
Telegram_bots <evbalbukova@gmail.com>
2021-08-25 00:05:03 +0300

    add methods

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import Optional
-
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def get_titles():
+    return JSONResponse({"status":"OK"})
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def difference_in_quotes(item_id: int, q: Optional[str] = None):
+    return JSONResponse({"status":"OK"})
